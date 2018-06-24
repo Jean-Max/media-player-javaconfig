a0945602414ce7b77cb847929467d0b5e2be472a
Jean-Max Marie-Sainte <jean-max-mariesainte@viseo.com>
2018-06-24 23:43:43 +0200

    Refactoring to use init callback method

--- src/main/java/com/viseo/formation/config/RandomMediaPlayerConfig.java
+++ src/main/java/com/viseo/formation/config/RandomMediaPlayerConfig.java
@@ -2,63 +2,20 @@ package com.viseo.formation.config;
 
 import com.viseo.formation.sound.IMediaFile;
 import com.viseo.formation.sound.IMediaPlayer;
-import com.viseo.formation.sound.MP3Player;
-import com.viseo.formation.sound.impl.*;
+import com.viseo.formation.sound.impl.MP3Player;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class RandomMediaPlayerConfig {
 
-
-
-    @Bean(name = "randomMP3")
-    public IMediaFile randomMediaFile(){
-
-        int choice = getRandomNumberInRange(0,5);
-        System.out.println("Audio file selected number: " + choice);
-        IMediaFile audioFile;
-
-        switch(choice){
-
-            case 0:
-                audioFile = new Soca2MP3File();
-                break;
-
-            case 1:
-                audioFile = new HipHopMP3File();
-                break;
-            case 2 :
-                audioFile = new SocaMP3File();
-                break;
-            case 3:
-                audioFile = new PopMP3File();
-                break;
-            case 4:
-                audioFile = new RockMP3File();
-                break;
-
-            case 5:
-                audioFile = new HipHop2MP3File();
-                break;
-
-            default:
-                audioFile =new PopMP3File();
-        }
-
-        return audioFile;
-    }
-
     @Bean (name = "randomMediaPlayer")
     public IMediaPlayer mediaPlayer(){
-        return new MP3Player(randomMediaFile());
+        MP3Player mp3Player = new MP3Player();
+        List<IMediaFile> mp3Playlist = mp3Player.getPlaylist();
+        return new MP3Player(mp3Playlist);
     }
 
-    private static int getRandomNumberInRange(int min, int max) {
-
-        if (min >= max) {
-            throw new IllegalArgumentException("max must be greater than min");
-        }
-        return (int)(Math.random() * ((max - min) + 1)) + min;
-    }
 }
---
+++ src/main/java/com/viseo/formation/sound/impl/MP3Player.java
@@ -0,0 +1,58 @@
+package com.viseo.formation.sound.impl;
+
+
+import com.viseo.formation.sound.IMediaFile;
+import com.viseo.formation.sound.IMediaPlayer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MP3Player implements IMediaPlayer {
+
+    private List<IMediaFile> playlist;
+
+    public MP3Player(){}
+
+    public MP3Player(List<IMediaFile> playlist){
+        this.playlist = playlist;
+    }
+
+    @PostConstruct
+    public void InitUserPlaylist(){
+        this.playlist = new ArrayList<IMediaFile>();
+        this.playlist.add(new SocaMP3File());
+        this.playlist.add(new Soca2MP3File());
+        this.playlist.add(new HipHopMP3File());
+        this.playlist.add(new HipHop2MP3File());
+        this.playlist.add(new PopMP3File());
+
+        System.out.println("User Media Playlist has been initialized with " + this.playlist.size() + " audio files!");
+    }
+
+
+    public String playMedia() {
+        final int index = getRandomNumberInRange(0, this.playlist.size() - 1);
+        System.out.println("Audio file selected number: " + index);
+        IMediaFile mediaFileSelected = this.playlist.get(index);
+        return mediaFileSelected.play();
+    }
+
+    @PreDestroy
+    public void DestroyUserPlaylist(){
+        this.playlist.clear();
+    }
+
+    public List<IMediaFile> getPlaylist() {
+        return playlist;
+    }
+
+    private static int getRandomNumberInRange(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+        return (int)(Math.random() * ((max - min) + 1)) + min;
+    }
+
+}
