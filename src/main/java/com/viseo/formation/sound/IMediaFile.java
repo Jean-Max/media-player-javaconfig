6d61001669f47651da25d09ec4dc462fb2b255bf
Jean-Max Marie-Sainte <jean-max-mariesainte@viseo.com>
2019-05-09 11:41:54 +0200

    Add java doc for interface methods

--- src/main/java/com/viseo/formation/sound/IMediaFile.java
+++ src/main/java/com/viseo/formation/sound/IMediaFile.java
@@ -5,13 +5,29 @@ package com.viseo.formation.sound;
  */
 public interface IMediaFile {
 
+    /**
+     * Play the media file
+     */
     String play();
 
+    /**
+     * Get the song title
+     * @return
+     */
     String getTitle();
 
+    /**
+     * Get the artist performing the song
+     */
     String getArtist();
 
+    /**
+     * Get the album related to this song.
+     */
     String getAlbum();
 
+    /**
+     * Get the song duration
+     */
     Integer getDuration();
 }
